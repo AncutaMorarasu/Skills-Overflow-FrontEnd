c6076a48e96c8f7fd31e9e2c3b6c1995db7de32f
AnnieisOK <morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.com>
2020-05-05 18:29:45 +0300

    added bootstrap

--- skills-overflow/src/App.tsx
+++ skills-overflow/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Skills Overflow</h1>
+      <h1 className="text-center">Hello Skills Overflow</h1>
+      <Button variant="info">info</Button>
     </div>
   );
 }
