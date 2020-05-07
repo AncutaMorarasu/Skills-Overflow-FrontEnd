12e9ebc8ecbd8f4dec78197fdc23e5a749271080
gheorghe <gicu.jianu@gmail.com>
2020-05-07 09:05:51 +0300

    password recovery page

--- skills-overflow/src/App.tsx
+++ skills-overflow/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFoundPage from "./pages/not-found-page";
 import LoginPage from "./pages/login-page";
+import PasswordRecPage from './pages/password-rec-page';
 
 function App() {
   return (
@@ -11,9 +12,12 @@ function App() {
         <Route exact path="/">
           <LoginPage />
         </Route>
-        <Route path="*">
+        <Route path="/not">
           <NotFoundPage />
         </Route>
+        <Route exact path="/recovery">
+        <PasswordRecPage/>  
+        </Route>  
       </Switch>
     </Router>
   );
---
+++ skills-overflow/src/pages/password-rec-page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+//import Button from "react-bootstrap/Button";
+
+function PasswordRecPage() {
+
+    return (
+        <div className="containerRecPage">
+            <h2 className="contentHeader">First, let's find your account</h2>
+            <div className="recPage">
+                <input id="input-char-counter" type="text" className="form-control" placeholder="Please enter your email address" />
+                <button type="button" className="btn btn-success btn-rounded">Submit</button>
+            </div>
+
+        </div>
+
+
+    );
+
+
+
+
+}
+
+export default PasswordRecPage;
