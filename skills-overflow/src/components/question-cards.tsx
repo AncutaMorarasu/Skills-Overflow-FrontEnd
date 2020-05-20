510f6b1edc4ceb3cae97b5be8906a7e762d0ea5d
AnnieisOK <morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.com>
2020-05-20 10:38:03 +0300

    all posts changes + CSS

--- skills-overflow/src/components/question-cards.tsx
+++ skills-overflow/src/components/question-cards.tsx
@@ -6,32 +6,47 @@ import { useHistory, useParams, useLocation } from "react-router-dom";
 import axios from "axios";
 
 function QuestionCard() {
-
   const history = useHistory();
-  const location = useLocation<{ topics: string[]}>();;
-  const {criteria, pageNo } = useParams();
+  const location = useLocation<{ topics: string[] }>();
+  const { criteria, pageNo } = useParams();
   //aici trebuie sa iei postarile
   const [questions, setQuestions] = useState({
     posts: [],
     totalPosts: 0
   });
   const [topics, setTopics] = useState({
-    topics:["Java", "Springboot", "SQL", "Tomcat", "JPA", "Google Cloud", "Hibernate", "CSS",
-  "Javascript", "Bootstrap", "Colaj manele", "Angular" , "JQuery"]
-  }) //nu folosesc niciodata setTopics, sunt hard-coded
-  const [filter, setFilters] = useState<{filterTopics:string[]}>({
-    filterTopics:[]
-  })
-  const [effects, setEffects] = useState({ef:true});
-  
-  const changeFlag = () =>{
-    if (effects.ef) setEffects({ef:false});
-    else {setEffects({ef:true})}
-  }
+    topics: [
+      "Java",
+      "Springboot",
+      "SQL",
+      "Tomcat",
+      "JPA",
+      "Google Cloud",
+      "Hibernate",
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Bootstrap",
+      "React",
+      "Angular",
+      "JQuery"
+    ]
+  }); //nu folosesc niciodata setTopics, sunt hard-coded
+  const [filter, setFilters] = useState<{ filterTopics: string[] }>({
+    filterTopics: []
+  });
+  const [effects, setEffects] = useState({ ef: true });
+
+  const changeFlag = () => {
+    if (effects.ef) setEffects({ ef: false });
+    else {
+      setEffects({ ef: true });
+    }
+  };
 
   let token = localStorage.getItem("user");
   let tokenCheck: any;
-  function localData(){
+  function localData() {
     if (typeof token === "string") {
       tokenCheck = JSON.parse(token);
     }
@@ -39,17 +54,21 @@ function QuestionCard() {
 
   useEffect(() => {
     localData();
-    let actualPageNo = (pageNo) ? pageNo :0;
-    let children = (location.state) ? location.state.topics :filter.filterTopics;
+    let actualPageNo = pageNo ? pageNo : 0;
+    let children = location.state ? location.state.topics : filter.filterTopics;
 
-    axios.post(`http://localhost:8081/allPosts/${actualPageNo}/${criteria}`, {topics:children}, 
-    {headers: {Authorization: 'Bearer ' + tokenCheck.token}})
-      .then( 
+    axios
+      .post(
+        `http://localhost:8081/allPosts/${actualPageNo}/${criteria}`,
+        { topics: children },
+        { headers: { Authorization: "Bearer " + tokenCheck.token } }
+      )
+      .then(
         response => {
           const object = response.data;
-          
-          setQuestions({ totalPosts:object[0], posts: object[1]});
-          setFilters({filterTopics:[]})
+
+          setQuestions({ totalPosts: object[0], posts: object[1] });
+          //  setFilters({ filterTopics: [] });
         },
         error => {
           console.log(error);
@@ -57,8 +76,11 @@ function QuestionCard() {
       );
   }, [effects]);
 
-  const renderPosts = questions.posts.map(({id, topics, title, body, numberOfComments, createDate, comments},
-      index) => {
+  const renderPosts = questions.posts.map(
+    (
+      { id, topics, title, body, numberOfComments, createDate, comments },
+      index
+    ) => {
       return (
         <Card className="cardText">
           <Card.Body>
@@ -72,15 +94,13 @@ function QuestionCard() {
                 {title}
               </a>
             </Card.Title>
-            <Card.Text>
-              {body}
-            </Card.Text>
+            <Card.Text>{body}</Card.Text>
             <Card.Text> Created on {createDate} </Card.Text>
             <Card.Text> Number of comments: {numberOfComments}</Card.Text>
             <Card.Text> Topic: {topics}</Card.Text>
-          </Card.Body> 
+          </Card.Body>
         </Card>
-      ); 
+      );
     }
   );
 
@@ -91,28 +111,27 @@ function QuestionCard() {
       if (array[i] === topicValue) {
         array.splice(i, 1);
         setFilters({ filterTopics: array });
-        console.log(array)
+        console.log(array);
         return;
       }
     }
     array.push(topicValue);
     setFilters({ filterTopics: array });
-    console.log(array)
-  }
+    console.log(array);
+  };
 
   return (
     <div className="questions">
       <QuestionModal />
-      <FilterSort 
-      topics={topics.topics} 
-      filterTopics = {filter.filterTopics}
-      onClick = {handleTopicClick} 
-      handleFlag = {changeFlag}
+      <FilterSort
+        topics={topics.topics}
+        filterTopics={filter.filterTopics}
+        onClick={handleTopicClick}
+        handleFlag={changeFlag}
       />
       {renderPosts}
     </div>
   );
-  
 }
 
 export default QuestionCard;
