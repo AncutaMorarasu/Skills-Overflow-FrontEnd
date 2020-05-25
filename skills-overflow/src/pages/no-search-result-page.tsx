d223e596b19f7193234986ae0f714100d3be8d6f
AnnieisOK <morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.comgit config --global user.email morarasu.ancuta@gmail.com>
2020-05-25 12:12:58 +0300

    ?

--- skills-overflow/src/pages/no-search-result-page.tsx
+++ skills-overflow/src/pages/no-search-result-page.tsx
@@ -3,29 +3,29 @@ import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import { Link, useLocation } from "react-router-dom";
 
-function NoSearchResult(){
+function NoSearchResult() {
 
-    //const {par} = props;
-    const location = useLocation();
-    const [par, setPar] = useState();
+  //const {par} = props;
+  const location = useLocation();
+  const [par, setPar] = useState();
 
-    useEffect(()=>{
-        const p = location.state;
-        const par = (p as any)?.par;
+  useEffect(() => {
+    const p = location.state;
+    const par = (p as any) ?.par;
 
-        console.log("location is --> " + par)
-        setPar(par);
-    })
+    console.log("location is --> " + par)
+    setPar(par);
+  })
 
- 
-    return (
-        <Container>
-          <Alert className="text-center" variant="warning">
-            Couldn't find what you're looking for... Perhaps you meant
-            <Link to="/dashboard" style={{color: 'black'}}> {par} </Link>?
+
+  return (
+    <Container>
+      <Alert className="text-center" variant="warning">
+        Couldn't find what you're looking for... Perhaps you meant
+            <Link to="/dashboard" style={{ color: 'black' }}> {par} </Link>?
           </Alert>
-        </Container>
-      );
+    </Container>
+  );
 }
 
 export default NoSearchResult;
